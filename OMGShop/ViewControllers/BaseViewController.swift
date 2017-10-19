a0920f95c2e9cce4ae3048244a82e27f3469bf4c
Unknown <mederic@omise.co>
2017-10-19 14:47:31 +0700

    Arrange folders

---
+++ OMGShop/ViewControllers/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  OMGShop
+//
+//  Created by Mederic Petit on 19/10/2560 BE.
+//  Copyright © 2560 Mederic Petit. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureView()
+    }
+
+    func configureView() {}
+
+}
--- OMGShop/ViewModels/BaseViewModel.swift
+++ OMGShop/ViewModels/BaseViewModel.swift
@@ -1,11 +1,13 @@
 //
-//  OMGShopUITests.swift
-//  OMGShopUITests
+//  BaseViewModel.swift
+//  OMGShop
 //
 //  Created by Mederic Petit on 19/10/2560 BE.
 //  Copyright © 2560 Mederic Petit. All rights reserved.
 //
 
-import XCTest
+import UIKit
 
-class OMGShopUITests: XCTestCase {}
+class BaseViewModel: NSObject {
+
+}
