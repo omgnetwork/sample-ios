f070d793b4b3530bfd823f6ea8950d72eea77d68
Unknown <mederic@omise.co>
2017-10-19 14:08:38 +0700

    Initial commit

---
+++ OMGShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OMGShop
+//
+//  Created by Mederic Petit on 19/10/2560 BE.
+//  Copyright © 2560 Mederic Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OMGShopTests/OMGShopTests.swift
@@ -0,0 +1,36 @@
+//
+//  OMGShopTests.swift
+//  OMGShopTests
+//
+//  Created by Mederic Petit on 19/10/2560 BE.
+//  Copyright © 2560 Mederic Petit. All rights reserved.
+//
+
+import XCTest
+@testable import OMGShop
+
+class OMGShopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
