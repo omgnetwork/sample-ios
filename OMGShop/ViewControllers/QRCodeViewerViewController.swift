233c88613373b4cb63a824c147c10e6e914ca3b3
mederic <mederic@omise.co>
2018-06-25 18:14:16 +0700

    Remove lazy from alertController

--- OMGShop/ViewControllers/QRCodeViewerViewController.swift
+++ OMGShop/ViewControllers/QRCodeViewerViewController.swift
@@ -15,23 +15,6 @@ class QRCodeViewerViewController: BaseViewController {
 
     @IBOutlet weak var waitingForScanLabel: UILabel!
 
-    lazy var alertController: UIAlertController = {
-        let alert = UIAlertController(title: self.viewModel.consumptionRequestTitle,
-                                      message: self.viewModel.consumptionRequestMessage(),
-                                      preferredStyle: .alert)
-        let rejectAction = UIAlertAction(title: self.viewModel.rejectButtonTitle, style: .destructive, handler: { [weak self] _ in
-            self?.viewModel.reject()
-            alert.dismiss(animated: true, completion: nil)
-        })
-        let approveAction = UIAlertAction(title: self.viewModel.approveButtonTitle, style: .default, handler: { [weak self] _ in
-            self?.viewModel.approve()
-            alert.dismiss(animated: true, completion: nil)
-        })
-        alert.addAction(rejectAction)
-        alert.addAction(approveAction)
-        return alert
-    }()
-
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.viewModel.stopListening()
@@ -46,13 +29,30 @@ class QRCodeViewerViewController: BaseViewController {
     override func configureViewModel() {
         super.configureViewModel()
         self.viewModel.onConsumptionRequest = {
-            self.present(self.alertController, animated: true, completion: nil)
+            self.present(self.setupAlertController(), animated: true, completion: nil)
         }
         self.viewModel.onFailApprove = { self.showError(withMessage: $0.message) }
         self.viewModel.onFailReject = { self.showError(withMessage: $0.message) }
         self.viewModel.onSuccessApprove = { self.showMessage($0) }
         self.viewModel.onSuccessReject = { self.showMessage($0) }
         self.viewModel.onLoadStateChange = { $0 ? self.showLoading() : self.hideLoading() }
+    }
+
+    func setupAlertController() -> UIAlertController {
+        let alert = UIAlertController(title: self.viewModel.consumptionRequestTitle,
+                                      message: self.viewModel.consumptionRequestMessage(),
+                                      preferredStyle: .alert)
+        let rejectAction = UIAlertAction(title: self.viewModel.rejectButtonTitle, style: .destructive, handler: { [weak self] _ in
+            self?.viewModel.reject()
+            alert.dismiss(animated: true, completion: nil)
+        })
+        let approveAction = UIAlertAction(title: self.viewModel.approveButtonTitle, style: .default, handler: { [weak self] _ in
+            self?.viewModel.approve()
+            alert.dismiss(animated: true, completion: nil)
+        })
+        alert.addAction(rejectAction)
+        alert.addAction(approveAction)
+        return alert
     }
 
 }
