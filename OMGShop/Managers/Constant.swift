63772bfe3f0e4f9fd636295194d49fb1e01f6eab
mederic <mederic@omise.co>
2018-07-05 13:56:36 +0700

    Remove comments

--- OMGShop/Managers/Constant.swift
+++ OMGShop/Managers/Constant.swift
@@ -53,19 +53,13 @@ enum AppState {
 }
 
 struct Constant {
-    static let omiseGOhostURL = "http://192.168.1.4:4000/api/client"
-    static let omiseGOSocketURL = "ws://192.168.1.4:4000/api/client/socket"
-    static let hostURL = "http://192.168.1.4:3000/api"
-    static let apiKeyId = "1"
-    static let apiKey = "dea3e25d6de6342a0976ba19d17f03ff"
-    static let omiseGOAPIKey = "JenwTqdp72_xMgfTaWiblF-_T5aX8GmfrgWhu6s3XdI"
     // Config
-//    static let omiseGOhostURL = "https://ewallet.demo.omisego.io/api/client"
-//    static let omiseGOSocketURL = "wss://ewallet.demo.omisego.io/api/client/socket"
-//    static let hostURL = "https://sample-shop.demo.omisego.io/api"
-//    static let apiKeyId = "1"
-//    static let apiKey = "f3629628db9316aa3710b2f705db59c1"
-//    static let omiseGOAPIKey = "zjH7vrLnwxuruQaDIZZ6jqKhlgLTsUCCYusBzUMQ3Ww"
+    static let omiseGOhostURL = "https://ewallet.demo.omisego.io/api/client"
+    static let omiseGOSocketURL = "wss://ewallet.demo.omisego.io/api/client/socket"
+    static let hostURL = "https://sample-shop.demo.omisego.io/api"
+    static let apiKeyId = "1"
+    static let apiKey = "f3629628db9316aa3710b2f705db59c1"
+    static let omiseGOAPIKey = "zjH7vrLnwxuruQaDIZZ6jqKhlgLTsUCCYusBzUMQ3Ww"
     static let authenticationScheme = "OMGShop"
 
     // Pagination
